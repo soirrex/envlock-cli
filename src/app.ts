bbcb59de3c8bd9e38fa4e8999d5ce9d25e180cdf
Soirrex <145297541+soirrex@users.noreply.github.com>
2026-04-17 16:11:53 +0300

    update version

--- src/app.ts
+++ src/app.ts
@@ -147,7 +147,7 @@ export class App {
   start() {
     this.program
       .name("el")
-      .version("1.0.2")
+      .version("1.0.3")
       .description(
         "CLI tool for secure management of .env templates. It stores templates locally, encrypts them with a master password, and allows quick project integration.",
       );
